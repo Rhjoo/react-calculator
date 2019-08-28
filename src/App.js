29fa229b905a6ec4ba5f6a521e5d90a1231e9aa2
Ryan Joo <rhjoo@Ryans-Mac-mini.local>
2019-08-27 22:01:00 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputState, setInputState] = useState("");
+  const [operator, setOperator] = useState();
+  const [firstNumber, setFirstNumber] = useState();
+  const [secondNumber, setSecondNumber] = useState();
+  const [equation, setEquation] = useState();  
+
+  const handleNumberClick = (value) => {
+    setInputState(inputState + value);
+  };
+  
+  const handleFirstEnterClick = () => {
+    setFirstNumber(inputState);
+  };
+  
+  const handleSecondEnterClick = () => {
+    setSecondNumber(inputState);
+  };
+  
+  const handleOperatorClick = (value) => {
+    setOperator(value);
+  };
+
+  const handleMakeEquationClick = () => {
+    setEquation({
+      firstNumber: firstNumber,
+      opeprator: operator,
+      secondNumber: secondNumber
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NumberButton value={7} handleNumberClick={handleNumberClick} />
+      <OperatorButton value={"+"} handleOperatorClick={handleOperatorClick} />
+      <FirstEnterButton handleFirstEnterClick={handleFirstEnterClick} />
+      <SecondEnterButton handleSecondEnterClick={handleSecondEnterClick} />
+      <MakeEquationButton handleMakeEquationClick={handleMakeEquationClick} />
+      <br />
+      {inputState} 
+      <br />
+      {firstNumber} {operator} {secondNumber}
     </div>
   );
-}
+};
+
+function NumberButton(props) {
+  return (
+    <button onClick={() => props.handleNumberClick(props.value)}>
+      7
+    </button>
+  );
+};
+
+function OperatorButton(props) {
+  return (
+    <button onClick={() => props.handleOperatorClick(props.value)}>
+      +
+    </button>
+  );
+};
+
+function FirstEnterButton(props) {
+  return (
+    <button onClick={() => props.handleFirstEnterClick()}>
+      Enter First Number
+    </button>
+  );
+};
+
+function SecondEnterButton(props) {
+  return (
+    <button onClick={() => props.handleSecondEnterClick()}>
+      Enter Second Number
+    </button>
+  );
+};
+
+function MakeEquationButton(props) {
+  return (
+    <button onClick={() => props.handleMakeEquationClick()}>
+      Make Equation
+    </button>
+  );
+};
 
 export default App;
+
