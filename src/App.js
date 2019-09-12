3d32c997a41707fb904f842aabd5b0e0df47c8de
Ryan Joo <rhjoo@Ryans-Mac-mini.local>
2019-09-12 00:25:50 -0700

    give credit to stackoverflow post for decimalPlaces

--- src/App.js
+++ src/App.js
@@ -108,11 +108,12 @@ function App() {
     };
   };
 
+  // copied from
+  // https://stackoverflow.com/questions/10454518/javascript-how-to-retrieve-the-number-of-decimals-of-a-string-number
   const decimalPlaces = (num) => {
     const match = (''+num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
     if (!match) { return 0; }
-    return Math.max(
-         0, (match[1] ? match[1].length : 0) - (match[2] ? +match[2] : 0));
+    return Math.max(0, (match[1] ? match[1].length : 0) - (match[2] ? +match[2] : 0));
   };
 
   const handleEqualClick = () => {
@@ -120,8 +121,6 @@ function App() {
     doMathClick();
   };
   
- 
-
   useEffect(() => {
     if (secondNumber !== undefined) {
       handleEqualClick();
